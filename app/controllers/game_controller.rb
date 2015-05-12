335dddaab07d0e9dae9eb7674da7a29708347e30
Wesley Merkel <ooesili@gmail.com>
2015-05-12 00:51:12 -0600

    Make the '/create' endpoint match the challenge

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,15 @@
 class GameController < ApplicationController
 
   def create
-    render json: Game.create
+    # create game and player
+    game = Game.create
+    player = game.players.create(nick: params[:nick])
+    # respond with appropriate data
+    render json: {
+      game_id: game.uuid,
+      player_id: player.uuid,
+      nick: player.nick,
+    }
   end
 
 end
