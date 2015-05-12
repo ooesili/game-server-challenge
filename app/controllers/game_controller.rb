396042d8757a1b88f0627ef4cbbf4db6991c15ee
Wesley Merkel <ooesili@gmail.com>
2015-05-12 01:01:58 -0600

    Allow users to join games through '/join'

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,4 +12,25 @@ class GameController < ApplicationController
     }
   end
 
+  def join
+    # try to find game
+    game = Game.find_by(uuid: params[:game_id])
+    if game
+      # game found
+      player = game.players.create(nick: params[:nick])
+      render json: {
+        registered: true,
+        player_id: player.uuid,
+        nick: player.nick,
+      }
+    else
+      # game not found
+      render status: :not_found, json: {
+        registered: false,
+        player_id: nil,
+        nick: nil,
+      }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/create' => 'game#create'
+  get '/join' => 'game#join'
 end
