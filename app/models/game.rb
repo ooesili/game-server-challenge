ec0d51b605c6c87c1bf7f49e985df2976bbac81a
Wesley Merkel <ooesili@gmail.com>
2015-05-16 23:43:39 -0600

    Remove an unused function from the Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -37,13 +37,6 @@ class Game < ActiveRecord::Base
 
   private
 
-  def set_defaults
-    self.board = NewGameHelper.new_game(15, 10)
-    self.uuid = SecureRandom.uuid
-  end
-
-  private
-
   def remove_creator
     self.creator_id = nil
     self.save!
